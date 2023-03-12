d2f3785ea059e3af2e4f20a54ed604caba9df0c7
Nimish Lakhmani <nimishlakhmani@gmail.com>
2023-03-12 14:49:21 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="f3814498-a7cc-4316-a016-7e9da63ee152"
+      userName="nimishlakhmani"
+      userSecret="12345"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
